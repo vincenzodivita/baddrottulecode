3ffaa97ff0d5ee54201b95094c7f8831c188d88f
vincenzodivita <divitavincenzo@icloud.com>
2024-05-24 02:14:51 +0200

    Add landing page

--- src/App.js
+++ src/App.js
@@ -5,19 +5,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>baddrottuleCode</h1>
+        <span>
+          Benvenuto su Baddrottole Code, il mio spazio personale dove condivido le mie avventure di programmazione!
+        </span>
+     
+        <h2>Un Gioco di Parole tra Cultura e Codice</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Il nome del mio sito, Baddrottole Code, è un gioco di parole che fonde la tradizione siciliana con un termine ben noto nel mondo della programmazione: spaghetti code.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Cos'è lo Spaghetti Code?</h2>
+        <p>
+          In informatica, il termine spaghetti code descrive un codice sorgente che è mal strutturato, difficile da seguire e da mantenere. Proprio come un piatto di spaghetti ingarbugliati, il codice spaghetti presenta un flusso di controllo caotico e intricato, rendendo complicata la comprensione e la modifica del programma.
+        </p>
+        <h2>Il Mio baddrottoleCode</h2>
+        <p>
+          Ho scelto di chiamare il mio sito Baddrottole Code per aggiungere un tocco di sicilianità al concetto di codice disordinato. Le baddrottole sono un termine dialettale siciliano, che ho scelto per rappresentare le mie radici. Anche se il mio sito non tratta di cucina, voglio evidenziare l'importanza di mantenere il codice ordinato e ben strutturato, pur riconoscendo che il mio codice inevitabilmente finirà per essere un po' disordinato – il mio "codice baddrottole".
+        </p>
+    
       </header>
+
     </div>
   );
 }
